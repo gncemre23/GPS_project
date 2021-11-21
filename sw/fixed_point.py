3c4b299b7281c6760e687e3244f0289292ae3a79
egoncu <gncemre23>
2021-11-21 22:22:43 +0300

    UPDATE : complete tracking python model

--- sw/fixed_point.py
+++ sw/fixed_point.py
@@ -28,8 +28,32 @@ def bin2float(bin_str, frac_len):
         frac_part += 0.5**(i+1) * int(bin_str[int_len+i])
     return minus*(int_part + frac_part)
 
+# to convert x to a.b . it is assumed that the scaling factor is known
+# Example: 1250 is obtained by 1.25 * 1000
+# So the scaling factor is 1000. By this function 1.25 is wanted to obtain
+# again.
+def int2fixedPoint(num, scale, frac_len, bit_len):
+    int_part = abs(num) // scale
+    frac_part = abs(num) - int_part * scale
+
+    int_str = bin(int_part)[2:].zfill(bit_len - frac_len)
+    frac_str = list(bin(0)[2:].zfill(frac_len))
+    for i in range(0, frac_len):
+        frac_part = frac_part*2
+        if(frac_part >= scale):
+            frac_str[i] = '1'
+            frac_part -= scale
+        else:
+            frac_str[i] = '0'
+    frac_str = ''.join(frac_str)
+    out = int_str+frac_str
+    if num < 0:
+        return twosComplement(int_str+frac_str)
+    return int_str+frac_str
 
 def float2bin(num, frac_len, bit_len):
+    # todo: remove this if conditon. Instead use abs of num for flooring
+    # todo: do the twosComplement at the end.
     if num < 0:
         int_part = abs(math.ceil(num))
         frac_part = -num - int_part
--- sw/tracking.py
+++ sw/tracking.py
@@ -1,3 +1,4 @@
+from os import ftruncate
 import fixed_point
 import math
 
@@ -33,17 +34,40 @@ import math
 # f=fixed_point.bin2float(e,20)
 # print(f)
 
+# num = 1250
+# print(fixed_point.int2fixedPoint(num,1000,10,16))
+
 # initial values
 
 # Fixed point parameters
 FRAC_LEN = 10
 BIT_LEN = 16
 
+# tau coefficients
+# FIX: fix this constant
+tau_carr_coef = 264.864
+
+tau_code_coef = 5.297 * 1000
+
+oldCarrErr = 0
+oldCarrNco = 0
+oldCodeErr = 0
+oldCodeNco = 0
+
+
+
 # normally it is 1,023e6. But the sampling freq is also having e6 factor, e6's are eliminated for simplicity
 # also the value is multiplied by 1000 for avoiding floation division
 codeFreq = 1023000
 carrFreq = 14500000
 samplingFreq = 53
+
+
+# DDS parameters for 32-bit phase input
+# 0.023 = 100 * 10 ^ 6 / 2 ^ 32
+freq_res = 0.023 * 1000
+phase_inc = carrFreq * 1000 // freq_res
+
 #remcodePhase = fixed_point.float2bin(0.0,20,32)
 remcodePhase = 0
 # -0.5
@@ -81,22 +105,22 @@ if int(text_file.readline()[0]) == 1:
 else:
     caCode[0] = 1
 
-three_bin = fixed_point.float2bin(3.0,FRAC_LEN,BIT_LEN)
+three_bin = fixed_point.float2bin(3.0, FRAC_LEN, BIT_LEN)
 
 # Initializing I_E, Q_E, I_L, Q_L, I_P, Q_P
-I_E_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
-Q_E_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
-I_L_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
-Q_L_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
-I_P_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
-Q_P_bin = fixed_point.float2bin(0.0,FRAC_LEN,BIT_LEN)
+I_E_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
+Q_E_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
+I_L_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
+Q_L_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
+I_P_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
+Q_P_bin = fixed_point.float2bin(0.0, FRAC_LEN, BIT_LEN)
 
 # for k in caCode:
 #     print(k)
 # ----------------------------------------------------------------------------------------
 
 
-for now do tracking only for one channel
+# for now do tracking only for one channel
 for ms in range(0, 36):
     #codePhaseStep = codeFreq / settings.samplingFreq;
     # integer division
@@ -149,12 +173,12 @@ for ms in range(0, 36):
         carrCos = math.cos(triarg)
         carrSin = math.cos(triarg)
 
-        carrCos_bin = fixed_point.float2bin(carrCos,FRAC_LEN,BIT_LEN)
-        carrSin_bin = fixed_point.float2bin(carrSin,FRAC_LEN,BIT_LEN)
-        
-        #least significant 2-bit of raw signal byte
-        raw = int.from_bytes(raw_file.read(1),"big") & 3
-        if raw == 0 :
+        carrCos_bin = fixed_point.float2bin(carrCos, FRAC_LEN, BIT_LEN)
+        carrSin_bin = fixed_point.float2bin(carrSin, FRAC_LEN, BIT_LEN)
+
+        # least significant 2-bit of raw signal byte
+        raw = int.from_bytes(raw_file.read(1), "big") & 3
+        if raw == 0:
             rawSignal = 1
         elif raw == 1:
             rawSignal = -1
@@ -162,11 +186,10 @@ for ms in range(0, 36):
             rawSignal = 3
         else:
             rawSignal = -3
-        
+
         # qBasebandSignal = carrCos * rawSignal
         # iBasebandSignal = carrSin * rawSignal
-            
-        
+
         if rawSignal == 1:
             qBasebandSignal_bin = carrCos_bin
             iBasebandSignal_bin = carrSin_bin
@@ -174,40 +197,158 @@ for ms in range(0, 36):
             qBasebandSignal_bin = fixed_point.twosComplement(carrCos_bin)
             iBasebandSignal_bin = fixed_point.twosComplement(carrSin_bin)
         else:
-            qBasebandSignal_bin = fixed_point.mul_float(carrCos_bin,three_bin,FRAC_LEN,FRAC_LEN,BIT_LEN)
-            qBasebandSignal_bin = fixed_point.mul_float(carrSin_bin,three_bin,FRAC_LEN,FRAC_LEN,BIT_LEN)
-        
-        if rawSignal == -3:
-            qBasebandSignal_bin = fixed_point.twosComplement(qBasebandSignal_bin)
-            iBasebandSignal_bin = fixed_point.twosComplement(iBasebandSignal_bin)
+            qBasebandSignal_bin = fixed_point.mul_float(
+                carrCos_bin, three_bin, FRAC_LEN, FRAC_LEN, BIT_LEN)
+            qBasebandSignal_bin = fixed_point.mul_float(
+                carrSin_bin, three_bin, FRAC_LEN, FRAC_LEN, BIT_LEN)
 
-        
+        if rawSignal == -3:
+            qBasebandSignal_bin = fixed_point.twosComplement(
+                qBasebandSignal_bin)
+            iBasebandSignal_bin = fixed_point.twosComplement(
+                iBasebandSignal_bin)
 
         # % Now get early, late, and prompt values for each
         # I_E = sum(earlyCode  .* iBasebandSignal)
         # Q_E = sum(earlyCode  .* qBasebandSignal)
         # I_P = sum(promptCode .* iBasebandSignal)
         # Q_P = sum(promptCode .* qBasebandSignal)
-        # I_L = sum(lateCode   .* iBasebandSignal)  
+        # I_L = sum(lateCode   .* iBasebandSignal)
         # Q_L = sum(lateCode   .* qBasebandSignal)
 
         if e_code == 1:
-            I_E_bin = fixed_point.add_float(I_E_bin,iBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_E_bin = fixed_point.add_float(Q_E_bin,qBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+            I_E_bin = fixed_point.add_float(
+                I_E_bin, iBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_E_bin = fixed_point.add_float(
+                Q_E_bin, qBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
         else:
-            I_E_bin = fixed_point.add_float(I_E_bin,fixed_point.twosComplement(iBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_E_bin = fixed_point.add_float(Q_E_bin,fixed_point.twosComplement(qBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+            I_E_bin = fixed_point.add_float(I_E_bin, fixed_point.twosComplement(
+                iBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_E_bin = fixed_point.add_float(Q_E_bin, fixed_point.twosComplement(
+                qBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
 
         if l_code == 1:
-            I_L_bin = fixed_point.add_float(I_L_bin,iBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_L_bin = fixed_point.add_float(Q_L_bin,qBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+            I_L_bin = fixed_point.add_float(
+                I_L_bin, iBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_L_bin = fixed_point.add_float(
+                Q_L_bin, qBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
         else:
-            I_L_bin = fixed_point.add_float(I_L_bin,fixed_point.twosComplement(iBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_L_bin = fixed_point.add_float(Q_L_bin,fixed_point.twosComplement(qBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-        
+            I_L_bin = fixed_point.add_float(I_L_bin, fixed_point.twosComplement(
+                iBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_L_bin = fixed_point.add_float(Q_L_bin, fixed_point.twosComplement(
+                qBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+
         if p_code == 1:
-            I_P_bin = fixed_point.add_float(I_P_bin,iBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_P_bin = fixed_point.add_float(Q_P_bin,qBasebandSignal_bin,FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+            I_P_bin = fixed_point.add_float(
+                I_P_bin, iBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_P_bin = fixed_point.add_float(
+                Q_P_bin, qBasebandSignal_bin, FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
         else:
-            I_P_bin = fixed_point.add_float(I_P_bin,fixed_point.twosComplement(iBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
-            Q_P_bin = fixed_point.add_float(Q_P_bin,fixed_point.twosComplement(qBasebandSignal_bin),FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+            I_P_bin = fixed_point.add_float(I_P_bin, fixed_point.twosComplement(
+                iBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+            Q_P_bin = fixed_point.add_float(Q_P_bin, fixed_point.twosComplement(
+                qBasebandSignal_bin), FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+    # end of the loop for 1ms
+
+    # ------Implement carrier loop discriminator (phase detector)----
+    # carrError = atan(Q_P / I_P) / (2.0 * pi);
+
+    # % Implement carrier loop filter and generate NCO command
+    # carrNco = oldCarrNco + (tau2carr/tau1carr) * ...
+    #     (carrError - oldCarrError) + carrError * (PDIcarr/tau1carr);
+    # oldCarrNco   = carrNco;
+    # oldCarrError = carrError;
+
+    # % Modify carrier freq based on NCO command
+    # carrFreq = carrFreqBasis + carrNco;
+
+    # trackResults(channelNr).carrFreq(loopCnt) = carrFreq;
+
+    # scaled by 1000
+    Q_P_int = fixed_point.bin2float(Q_P_bin, 2*FRAC_LEN) * 1000 * 1000
+    I_P_int = fixed_point.bin2float(I_P_bin, 2*FRAC_LEN) * 1000
+
+    # this division result is 1000 times the real division value
+    div_Q_P_I_P_int = Q_P_int // I_P_int
+    # the division value is converted to fixed point number with 14*bit fractal part
+    # since the cordic accepts the input type fix_14_16
+    div_Q_P_I_P = fixed_point.int2fixedPoint(div_Q_P_I_P_int, 1000, 14, 16)
+    arc_tan = math.atan(div_Q_P_I_P)
+
+    # scale the arc_tan result by 1000000
+    arc_tan_int = arc_tan * 1000000
+    # scale the 2*pi by 1000
+    pi_2_int = math.pi * 2 * 1000
+
+    # integer division giving the result arc_tan/2*pi * 1000
+    # which is the 1000 times carrErr having the same level with carrFreq
+    carrErr = arc_tan_int // pi_2_int
+
+    # ignore pdi part
+    diff = carrErr - oldCarrErr
+    diff_bin = fixed_point.float2bin(diff, 2*FRAC_LEN, 2*BIT_LEN)
+    tau_coeff_bin = fixed_point.float2bin(tau_carr_coef, 2*FRAC_LEN, 2*BIT_LEN)
+
+    oldCarrErr 
+
+    diff_mult_bin = fixed_point.mul_float(
+        diff_bin, tau_coeff_bin, 2*FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+
+    oldCarrNco_bin = fixed_point.float2bin(oldCarrNco,2*FRAC_LEN)
+    carrNco_bin = fixed_point.add_float(
+        oldCarrNco_bin, diff_mult_bin, 2*FRAC_LEN, 2*FRAC_LEN, 2*BIT_LEN)
+
+    carrNco = int(fixed_point.bin2float(carrNco_bin,2*FRAC_LEN))
+    
+    # DDS phase inc(PINC) input
+    phase_inc += carrNco // freq_res
+    carrFreq = (phase_inc * freq_res) / 1000.0
+
+    oldCarrNco = carrNco
+    oldCarrErr = carrErr
+    #     (carrError - oldCarrError) + carrError * (PDIcarr/tau1carr);
+    # -----------------------------------------------------------------
+    
+    # -------- Find DLL error and update code NCO -------------------------------------
+    # codeError = (sqrt(I_E * I_E + Q_E * Q_E) - sqrt(I_L * I_L + Q_L * Q_L)) / ...
+    #     (sqrt(I_E * I_E + Q_E * Q_E) + sqrt(I_L * I_L + Q_L * Q_L));
+    
+    # % Implement code loop filter and generate NCO command
+    # codeNco = oldCodeNco + (tau2code/tau1code) * ...
+    #     (codeError - oldCodeError) + codeError * (PDIcode/tau1code);
+    # oldCodeNco   = codeNco;
+    # oldCodeError = codeError;
+    
+    # % Modify code freq based on NCO command
+    # codeFreq = settings.codeFreqBasis - codeNco;
+    
+    # trackResults(channelNr).codeFreq(loopCnt) = codeFreq;
+    
+    I_E_bin_sqr = fixed_point.mul_float(I_E_bin,I_E_bin,2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+    Q_E_bin_sqr = fixed_point.mul_float(Q_E_bin,Q_E_bin,2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+
+    I_L_bin_sqr = fixed_point.mul_float(I_L_bin,I_L_bin,2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+    Q_L_bin_sqr = fixed_point.mul_float(Q_L_bin,Q_L_bin,2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+
+    I_E_sqr_plus_Q_E_sqr = fixed_point.add_float(I_E_bin_sqr,Q_E_bin_sqr,2*FRAC_LEN, 2*BIT_LEN)
+    I_L_sqr_plus_Q_L_sqr = fixed_point.add_float(I_L_bin_sqr,Q_L_bin_sqr,2*FRAC_LEN, 2*BIT_LEN)
+
+    t_bin = fixed_point.float2bin(10000,2*FRAC_LEN,2*BIT_LEN)
+    # scale by 10000 for integer sqrt operation
+    sqr_int_0 = fixed_point.mul_float(I_E_sqr_plus_Q_E_sqr, t_bin, 2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+    sqr_int_1 = fixed_point.mul_float(I_L_sqr_plus_Q_L_sqr, t_bin, 2*FRAC_LEN,2*FRAC_LEN,2*BIT_LEN)
+
+    # Cordic will be used for sqrt 
+    # square roots are 100 times the real value because of 10000 scaling factor.
+    sqrt_int_0 = math.sqrt(sqr_int_0)
+    sqrt_int_1 = math.sqrt(sqr_int_1)
+
+    codeErr = (sqrt_int_0 - sqrt_int_1) * 1000 // (sqrt_int_0 + sqrt_int_1)
+
+    codeNco  = oldCodeNco + ((codeErr - oldCodeErr) * tau_code_coef) // 1000 
+
+    codeFreq -= codeNco
+    oldCodeNco = codeNco
+    oldCodeErr = codeErr
+
+    # ------------------------------------------------------------------------------
